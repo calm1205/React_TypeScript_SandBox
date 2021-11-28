1099482f98880858628869e1f9e1ec90979ebaf2
calm1205 <naginani072@gmail.com>
2021-11-28 17:31:36 +0900

    prettier format

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const App = () => {
   return (
     <div className="App">
       <p>Learn React</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
